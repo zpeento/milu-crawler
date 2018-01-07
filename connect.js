d9a521bbef75f950e297f4d09b0170d76ffcca0b
moose.zhou <509887158@qq.com>
2018-01-08 00:38:01 +0800

    [upgrade]项目初始化

---
+++ connect.js
@@ -0,0 +1,4 @@
+const mongodb = require('mongodb');
+const connect = mongodb.MongoClient.connect;
+
+module.exports = connect;
--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 const superagent = require('superagent');
 const cheerio = require('cheerio');
 
+
 const baseUrl = 'http://jandan.net/duan';
 
-superagent.get(baseUrl)
-    .end((err,res) => {
-        if(err) {
-            return console.log(err);
-        }
-        let $ = cheerio.load(res.text);
-        let content = $('html').html();
+const connect = require('./connect.js');
 
-        console.log(content);
-    })
+connect("mongodb://localhost:27017/test")
+    .then(
+        async (db)=>{
+            const collection = db.collection('milu_crawler');
+            const res = await collection.insertOne({name: "test", age: 19});
+            const result = await collection.find({}).toArray();
+            console.log(result);        
+            db.close();
+        },
+        (err)=>{
+            console.log("db connection error")
+            throw err;
+        }
+    )
+// superagent.get(baseUrl)
+//     .end((err,res) => {
+//         if(err) {
+//             return console.log(err);
+//         }
+//         let $ = cheerio.load(res.text);
+//         let content = $('html').html();
+
+//         console.log(content);
+//     })
